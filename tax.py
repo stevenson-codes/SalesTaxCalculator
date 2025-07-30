8480437e4de778acedf523816b46c312c931661f
your-username <your-email>
2025-07-30 16:54:26 -0700

    Added input and output

--- tax.py
+++ tax.py
@@ -1,4 +1,4 @@
-amount = 100
+amount = float(input('> Please enter the amount\n> '))
 tax = 0.12
-total = amount + (amount * tax)
-print(total)
+total = amount + amount * tax
+print(f'> This it the total amount with tax: {total}')
